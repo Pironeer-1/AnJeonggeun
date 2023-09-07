3c252605d1ca6fa3acfac7c3451a84c7483825b6
ajroot5685 <anjeong8577@naver.com>
2023-09-07 23:38:22 +0900

    19 App - 오류 페이지와 홈페이지 구현

--- Node.js/main.js
+++ Node.js/main.js
@@ -4,40 +4,68 @@ var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    var queryData = url.parse(_url,true).query; // _url을 객체로 바꾸어준다.
-    console.log(queryData.id);
-    var title = queryData.id;
-    if(_url == '/'){
-        title='Welcome';
+    var queryData = url.parse(_url,true).query; // _url을 객체로 바꾸어준다. .query : url 정보 중에서 query 정보만 가져옴
+    var pathname = url.parse(_url, true).pathname;
+
+    console.log(url.parse(_url,true));
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var template = `
+            <!doctype html>
+            <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html>
+            `;
+            response.writeHead(200);    // 응답 헤더에 대한 정보 기록 200:정상
+            response.end(template);
+        }
+        else{
+            // 읽은 파일은 decription에 저장됨
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                </html>
+                `;
+                response.writeHead(200);    // 응답 헤더에 대한 정보 기록 200:정상
+                response.end(template);
+            });
+        }
+    }else{
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    // 읽은 파일은 decription에 저장됨
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-        var template = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-        </html>
-        `;
-        response.end(template);
-    });
     
- 
 });
 app.listen(3000);
