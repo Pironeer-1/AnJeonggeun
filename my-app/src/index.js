f00f616de217d6372173f0105f3abd9068db4f0f
ajroot5685 <anjeong8577@naver.com>
2023-09-11 14:06:23 +0900

    react 7장 훅

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList/>
+    <Accommodate/>
   </React.StrictMode>,
   document.getElementById('root')
 )
@@ -17,6 +17,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 6장
+// import NotificationList from './chapter_06/NotificationList';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <NotificationList/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 
 // 5장
 // import CommentList from './chapter_05/CommentList';
