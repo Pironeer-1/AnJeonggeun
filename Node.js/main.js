0ccc291c2f326083ba41a5cb1690bd77d8f5b0d9
ajroot5685 <anjeong8577@naver.com>
2023-09-12 21:51:37 +0900

    32장 App - POST 방식으로 전송된 데이터 받기

--- Node.js/main.js
+++ Node.js/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
     return `
@@ -76,8 +77,22 @@ var app = http.createServer(function(request,response){
                     </p>
                 </form>
             `);
-            response.writeHead(200);    // 응답 헤더에 대한 정보 기록 200:정상
+            response.writeHead(200);
             response.end(template);
+        });
+    }else if(pathname === '/create_process'){
+        var body = '';
+
+        request.on('data', function (data) {
+            body = body + data;
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(title);
+            console.log(description);
         });
     }else{
         response.writeHead(404);
