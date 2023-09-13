16153734e94585099ec49211f8478a4647f2e086
ajroot5685 <anjeong8577@naver.com>
2023-09-13 22:08:46 +0900

    47장 App - 출력 정보에 대한 보안

--- Node.js/main.js
+++ Node.js/main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -28,7 +29,6 @@ var app = http.createServer(function(request,response){
         }
         else{
             fs.readdir('./data', function(error, filelist) {  // readdir : 해당 디렉토리에 있는 파일 목록을 배열로 반환
-                var list = template.list(filelist);
                 var filteredId = path.parse(queryData.id).base;
                 // console.log(path.parse(queryData.id));
                 // console.log(filteredId);
@@ -36,12 +36,17 @@ var app = http.createServer(function(request,response){
                 // 읽은 파일은 decription에 저장됨
                 fs.readFile(`data/${filteredId}`, 'utf8', function(err,description){
                     var title = queryData.id;
-                    var html = template.HTML(title, list,
-                        `<h2>${title}</h2><p>${description}</p>`,
+                    var sanitizedTitle = sanitizeHtml(title);
+                    var sanitizedDescription = sanitizeHtml(description, {
+                        allowedTags:['h1']
+                    });
+                    var list = template.list(filelist);
+                    var html = template.HTML(sanitizedTitle, list,
+                        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
                         `<a href="/create">create</a>
-                        <a href="/update?id=${title}">update</a>
+                        <a href="/update?id=${sanitizedTitle}">update</a>
                         <form action="delete_process" method="post" onsubmit="return confirm('정말로 삭제하시겠습니까?');">
-                            <input type="hidden" name="id" value="${title}">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
                             <input type="submit" value="delete">
                         </form>`
                         );
