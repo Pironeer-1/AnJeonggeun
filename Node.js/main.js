3e66ff57315cf3665417f056557925c09a6d18fe
ajroot5685 <anjeong8577@naver.com>
2023-09-13 21:44:22 +0900

    46장 App - 입력 정보에 대한 보안

--- Node.js/main.js
+++ Node.js/main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -28,8 +29,12 @@ var app = http.createServer(function(request,response){
         else{
             fs.readdir('./data', function(error, filelist) {  // readdir : 해당 디렉토리에 있는 파일 목록을 배열로 반환
                 var list = template.list(filelist);
+                var filteredId = path.parse(queryData.id).base;
+                // console.log(path.parse(queryData.id));
+                // console.log(filteredId);
+                // console.log(queryData.id);
                 // 읽은 파일은 decription에 저장됨
-                fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+                fs.readFile(`data/${filteredId}`, 'utf8', function(err,description){
                     var title = queryData.id;
                     var html = template.HTML(title, list,
                         `<h2>${title}</h2><p>${description}</p>`,
@@ -86,8 +91,9 @@ var app = http.createServer(function(request,response){
     }else if(pathname === '/update'){
         fs.readdir('./data', function(error, filelist) {  // readdir : 해당 디렉토리에 있는 파일 목록을 배열로 반환
             var list = template.list(filelist);
+            var filteredId = path.parse(queryData.id).base;
             // 읽은 파일은 decription에 저장됨
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err,description){
                 var title = queryData.id;
                 var html = template.HTML(title, list,
                     `
@@ -138,7 +144,8 @@ var app = http.createServer(function(request,response){
         request.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`, function(error){
+            var filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, function(error){
                 response.writeHead(302, {Location: `/`});
                 response.end();
             });
