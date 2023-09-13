ea002ac77703eefd69e4a8c4261c01c832554d5a
ajroot5685 <anjeong8577@naver.com>
2023-09-13 14:36:03 +0900

    34장 App - 글 수정(수정 링크 생성)

--- Node.js/main.js
+++ Node.js/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
     return `
     <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
         <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
-            <a href="/create">create</a>
+            ${control}
             ${body}
         </body>
     </html>
@@ -45,7 +45,10 @@ var app = http.createServer(function(request,response){
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
                 var list = templateList(filelist);
-                var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                var template = templateHTML(title, list,
+                    `<h2>${title}</h2><p>${description}</p>`,
+                    `<a href="/create">create</a>`
+                    );
                 response.writeHead(200);    // 응답 헤더에 대한 정보 기록 200:정상
                 response.end(template);
             });
@@ -56,7 +59,10 @@ var app = http.createServer(function(request,response){
                 // 읽은 파일은 decription에 저장됨
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
                     var title = queryData.id;
-                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                    var template = templateHTML(title, list,
+                        `<h2>${title}</h2><p>${description}</p>`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        );
                     response.writeHead(200);    // 응답 헤더에 대한 정보 기록 200:정상
                     response.end(template);
                 });
