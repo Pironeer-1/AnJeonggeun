7980e0d8f32fe1586f3335c870bd18df16bbbd58
ajroot5685 <anjeong8577@naver.com>
2023-09-14 23:59:23 +0900

    0914 스터디

--- my-app/src/chapter_10/AttendanceBook.jsx
+++ my-app/src/chapter_10/AttendanceBook.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const students = [
-    {
-        id: 1,
-        name: "Inje",
-    },
-    {
-        id: 2,
-        name: "Steve",
-    },
-    {
-        id: 3,
-        name: "Bill",
-    },
-    {
-        id: 4,
-        name: "Jeff",
-    },
-];
+// const students = [
+//     {
+//         id: 1,
+//         name: "Inje",
+//     },
+//     {
+//         id: 2,
+//         name: "Steve",
+//     },
+//     {
+//         id: 3,
+//         name: "Bill",
+//     },
+//     {
+//         id: 4,
+//         name: "Jeff",
+//     },
+// ];
 
-function AttendanceBook(props){
-    // 리스트 안에서 고유한 키값 필수
+// function AttendanceBook(props){
+//     // 리스트 안에서 고유한 키값 필수
+    // return(
+    //     <ul>
+    //         {students.map((student)=>{
+    //             return <li key={student.id}>{student.name}</li>
+    //         })}
+    //     </ul>
+    // );
+// }
+
+const AttendanceBook = (props)=>{
+    const [students, setStudents] = useState([]);
+
+    useEffect(()=>{
+        fetch("/students.json",{
+            method: "GET",
+        })
+        .then((response)=>response.json())
+        .then((result)=>{
+            setStudents(result);
+            console.log(result);
+        });
+    }, []);
+
     return(
         <ul>
             {students.map((student)=>{
