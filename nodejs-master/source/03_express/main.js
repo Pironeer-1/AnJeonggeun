074873d05d19f8a1efc235dc925498af0ba509c5
ajroot5685 <anjeong8577@naver.com>
2023-09-22 13:33:10 +0900

    04 홈페이지 구현

--- nodejs-master/source/03_express/main.js
+++ nodejs-master/source/03_express/main.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
 
-app.get('/', (req,res) => res.send('Hello world!'))
-app.listen(3000, ()=> console.log('Example app listening on port 3000!'))
+app.get('/', function(request, response) {
+    fs.readdir('./data', function(error, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+        );
+        response.send(html);
+    });
+});
+app.get('/page', function(req, res) {
+    return res.send('/page')
+});
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+});
 
 // console.log('Hello no deamon');
 // var http = require('http');
